fcd673245bf44dd02bbef17bcc7fbae226e5aca7
sarahvenus <73270035+sarahvenus@users.noreply.github.com>
2020-12-02 13:24:06 -0800

    Update app.js

--- simple-express-crud/app.js
+++ simple-express-crud/app.js
@@ -17,7 +17,7 @@ app.use(
 )
 
 app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
+  response.json({ info: 'Node.js, Express, and Postgres API2' })
 })
 
 app.get('/users', db.getUsers)
